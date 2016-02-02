b3f87794b76d4ab0677c30d5b08b5dbb038b1768
Dimas Moreira Junior <dimasmjunior@gmail.com>
2016-02-01 23:35:16 -0200

    Simplify dependency injection.

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,9 @@ var gulp = require('gulp');
 var mainBowerFiles = require('main-bower-files');
 var inject = require('gulp-inject');
 
-gulp.task('default', [
-    'copy-index-html',
-    'copy-libs-js',
-    'copy-libs-css',
-    'inject-libs']);
-
-gulp.task('copy-index-html', function() {
-    return gulp.src('public/index.html')
-        .pipe(gulp.dest('public/dist'));
-});
-
-gulp.task('copy-libs-js', function() {
-    return gulp.src(mainBowerFiles(/\.js$/))
-        .pipe(gulp.dest('public/dist/libs/js'));
-});
-
-gulp.task('copy-libs-css', function() {
-    return gulp.src(mainBowerFiles(/\.css$/))
-        .pipe(gulp.dest('public/dist/libs/css'));
-});
-
-gulp.task('inject-libs', function() {
-    var target = gulp.src('public/dist/index.html');
-    var sources = gulp.src([
-        'public/dist/libs/js/*.js',
-        'public/dist/libs/css/*.css'], {read: false});
-    return target.pipe(inject(sources, {relative: true}))
-      .pipe(gulp.dest('public/dist'));
+gulp.task('default', function() {
+    var libs = gulp.src(mainBowerFiles(), {read: false});
+    return gulp.src('client/index.html')
+        .pipe(inject(libs, {relative: true}))
+        .pipe(gulp.dest('client'));
 });
