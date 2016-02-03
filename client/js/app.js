31024195d2833eac5844257e7f8153884ceafcd0
Dimas Moreira Junior <dimasmjunior@gmail.com>
2016-02-02 23:35:12 -0200

    Add basic Angular controller

--- client/js/app.js
+++ client/js/app.js
@@ -1,3 +1,10 @@
 (function () {
   var app = angular.module('ankyra', []);
+  app.controller('ReviewController', function() {
+    this.card = card;
+  });
+  var card = {
+    front: '1 + 1',
+    back: '2'
+  };
 })();
