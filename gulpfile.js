89688f2e9e0de0b84e82751831d7282363d2a98c
Dimas Moreira Junior <dimasmjunior@gmail.com>
2016-02-07 23:16:55 -0200

    Add eslint to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var mainBowerFiles = require('main-bower-files');
 var inject = require('gulp-inject');
+var eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
     var libs = gulp.src(mainBowerFiles(), {read: false});
     return gulp.src('client/index.html')
         .pipe(inject(libs, {relative: true}))
         .pipe(gulp.dest('client'));
+});
+
+gulp.task('lint', function () {
+    return gulp.src(['**/*.js', '!node_modules/**', '!client/bower_components/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
